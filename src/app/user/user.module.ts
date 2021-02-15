12bc14a63b834de8973a3683bc5dca94835d4d70
Lauramars <lauramars85@gmail.com>
2021-02-15 10:59:14 +0100

    Add user module/components/model/service + user Roure

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { UserService } from '../user/user.service';
+
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
+
+const routes: Routes = [
+  {
+    path: 'me',
+    component: UserDashboardComponent,
+    data: { title: 'Dashboard' },
+  },
+  { path: 'users', component: UserListComponent, data: { title: 'Users' } },
+  {
+    path: 'profile',
+    component: UserDetailComponent,
+    data: { title: 'Profile' },
+  },
+];
+
+@NgModule({
+  declarations: [
+    UserDashboardComponent,
+    UserDetailComponent,
+    UserListComponent,
+    UserListItemComponent,
+  ],
+  imports: [SharedModule, RouterModule.forChild(routes)],
+  providers: [UserService],
+})
+export class UserModule {}
